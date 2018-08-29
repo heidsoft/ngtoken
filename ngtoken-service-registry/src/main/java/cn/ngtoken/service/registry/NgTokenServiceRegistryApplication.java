e956bd86cf93de9fa2d73286585aeec213492a15
Bin Liu <bin.liu@magicwindow.cn>
2018-08-29 19:53:01 +0800

    ngtoken

--- ngtoken-service-registry/src/main/java/cn/ngtoken/service/registry/NgTokenServiceRegistryApplication.java
+++ ngtoken-service-registry/src/main/java/cn/ngtoken/service/registry/NgTokenServiceRegistryApplication.java
@@ -1,3 +1,5 @@
+package cn.ngtoken.service.registry;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
