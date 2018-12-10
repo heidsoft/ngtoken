457cc500895ce4c229bfc87f6ca8225409fa0328
Bin Liu <bin.liu@magicwindow.cn>
2018-12-10 11:59:23 +0800

    add kong gateway test

---
+++ ngtoken-kong/src/main/java/cn/ngtoken/kong/NgtokenKongApplication.java
@@ -0,0 +1,12 @@
+package cn.ngtoken.kong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgtokenKongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgtokenKongApplication.class, args);
+	}
+}
